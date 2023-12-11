804f9bb29f87de00002cd1b56e992cf254c5c96c
Almir Firmo <almir.filho77@gmail.com>
2023-12-11 15:30:10 -0300

    Implement Double Linked List entirely

--- DoubleLinkedList/inc/DoubleLinkedList.hpp
+++ DoubleLinkedList/inc/DoubleLinkedList.hpp
@@ -20,7 +20,6 @@ public:
     void Prepend(Ty value);
     void PopBack();
     void PopFront();
-    void Reverse();
     void PrintList() const;
 
     bool Insert(Ty value, int index);
@@ -73,7 +72,7 @@ void DoubleLinkedList<Ty>::Append(Ty value)
 }
 
 template <class Ty>
-inline void DoubleLinkedList<Ty>::PopBack()
+void DoubleLinkedList<Ty>::PopBack()
 {
     if (m_length == 0)
     {
@@ -94,6 +93,143 @@ inline void DoubleLinkedList<Ty>::PopBack()
     }
     delete temp;
     m_length--;
+}
+
+template <class Ty>
+void DoubleLinkedList<Ty>::Prepend(Ty value)
+{
+    Node<Ty> *newNode = new Node<Ty>(value);
+    if (m_length == 0)
+    {
+        m_head = newNode;
+        m_tail = newNode;
+    }
+    else
+    {
+        newNode->next = m_head;
+        m_head->prev = newNode;
+        m_head = newNode;
+    }
+    m_length++;
+}
+
+template <class Ty>
+void DoubleLinkedList<Ty>::PopFront()
+{
+    if (m_length == 0)
+    {
+        return;
+    }
+
+    Node<Ty> *temp = m_head;
+    if (m_length == 1)
+    {
+        m_head = nullptr;
+        m_tail = nullptr;
+    }
+    else
+    {
+        m_head = m_head->next;
+        m_head->prev = nullptr;
+    }
+    delete temp;
+    m_length--;
+}
+
+template <class Ty>
+Node<Ty> *DoubleLinkedList<Ty>::GetNode(int index)
+{
+    if (index < 0 || index >= m_length)
+    {
+        return nullptr;
+    }
+
+    Node<Ty> *temp;
+    if (index < m_length/2)
+    {
+        temp = m_head;
+        for (int i=0; i<index; i++)
+        {
+            temp = temp->next;
+        }
+    }
+    else
+    {
+        temp = m_tail;
+        for (int i=m_length-1; i>index; i--)
+        {
+            temp = temp->prev;
+        }
+    }
+    return temp;
+}
+
+template <class Ty>
+bool DoubleLinkedList<Ty>::SetNode(int index, Ty value)
+{
+    auto temp = GetNode(index);
+    if (temp == nullptr)
+    {
+        std::cerr << "Invalid index!\n";
+        return false;
+    }
+    temp->value = value;
+    return true;
+}
+
+template <class Ty>
+bool DoubleLinkedList<Ty>::Insert(Ty value, int index)
+{
+    if (index < 0 || index >= m_length)
+    {
+        return false;
+    }
+
+    if (index == 0)
+    {
+        Prepend(value);
+        return true;
+    }
+
+    if (index == m_length)
+    {
+        Append(value);
+        return true;
+    }
+
+    auto before = GetNode(index-1);
+    auto after = before->next;
+    auto newNode = new Node(value);
+    before->next = newNode;
+    after->prev = newNode;
+    newNode->next = after;
+    newNode->prev = before;
+    m_length++;
+    return true;
+}
+
+template <class Ty>
+bool DoubleLinkedList<Ty>::Delete(int index)
+{
+    auto temp = GetNode(index);
+    if (temp == nullptr)
+    {
+        return false;
+    }
+
+    if (m_length == 1)
+    {
+        m_head == nullptr;
+        m_tail == nullptr;
+    }
+    else
+    {
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+    }
+    delete temp;
+    m_length--;
+    return true;
 }
 
 template <class Ty>
--- DoubleLinkedList/src/main.cpp
+++ DoubleLinkedList/src/main.cpp
@@ -23,5 +23,57 @@ int main(int argc, char **argv)
     dll.PopBack();
     dll.PrintList();
 
+    std::cout << "Prepend!\n";
+    dll.Prepend(2);
+    dll.PrintList();
+    std::cout << "Prepend again!\n";
+    dll.Prepend(1);
+    dll.PrintList();
+
+    std::cout << "Pop Front!\n";
+    dll.PopFront();
+    dll.PrintList();
+    std::cout << "Pop Front again!\n";
+    dll.PopFront();
+    dll.PrintList();
+    std::cout << "Pop Front again!\n";
+    dll.PopFront();
+    dll.PrintList();
+
+    std::cout << "New List:\n";
+    dll.Append(1);
+    dll.Append(2);
+    dll.Append(3);
+    dll.Append(4);
+    dll.PrintList();
+    std::cout << "Get at index 1: " << dll.GetNode(1)->value << '\n';
+    std::cout << "Get at index 2: " << dll.GetNode(2)->value << '\n';
+
+    std::cout << "Set value at index -1 to 97 (should give error) \n";
+    dll.SetNode(-1, 97);
+    std::cout << "Set value at index 0 to 98\n";
+    dll.SetNode(0, 98);
+    std::cout << "Set value at index 1 to 100\n";
+    dll.SetNode(1, 100);
+    std::cout << "Set value at index 2 to 101\n";
+    dll.SetNode(2, 101);
+    std::cout << "Set value at index 3 to 102\n";
+    dll.SetNode(3, 103);
+    std::cout << "Set value at index 4 to 104 (should give error) \n";
+    dll.SetNode(4, 104);
+    dll.PrintList();
+
+    std::cout << "Insert 99 at index 1\n";
+    dll.Insert(99, 1);
+    std::cout << "Insert 102 at index 3\n";
+    dll.Insert(102, 4);
+    dll.PrintList();
+
+    std::cout << "Delete at index 1\n";
+    dll.Delete(1);
+    std::cout << "Delete at index 3\n";
+    dll.Delete(3);
+    dll.PrintList();
+
     return 0;
 }
